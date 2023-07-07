b78ab237cde8d082fc4d2f4162bcac1be55d55ed
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-07-07 12:16:18 -0600

    Create UsingGit.sh

---
+++ UsingGit.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/GauravWalia19/TETRIS.git
+ll
+cd TETRIS/
+ll
+cd build/
+ll
+bash build.bash
+sudo yum install java java-devel -y
+bash build.bash
+cd ..
+bash test.bash
+ll
+bash tetris.bash
+history
